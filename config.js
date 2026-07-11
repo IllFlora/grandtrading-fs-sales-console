2ef7d15d554830209a385d5d0d5b784c805e2d70
IllFlora <149352334+IllFlora@users.noreply.github.com>
2026-07-11 16:22:43 +0900

    Add config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+window.GT_CONFIG={
+  googleClientId:'REPLACE_WITH_GOOGLE_OAUTH_CLIENT_ID.apps.googleusercontent.com',
+  spreadsheetId:'1F84uPktW-sY8Sf3riBHxUw_NZAymTGHOMEI_7DgfVnE',
+  masterSheet:'01_リードマスター',
+  activitySheet:'02_活動履歴',
+  refreshSeconds:60
+};
+
